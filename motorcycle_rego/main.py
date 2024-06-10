035410a8068af33a211ef30c9f46efdc4761e7b3
PUffCoder <heyhyi31@gmail.com>
2024-06-10 22:24:09 +1200

    davde

--- motorcycle_rego/main.py
+++ motorcycle_rego/main.py
@@ -1,86 +1,71 @@
 import os
-
+from ultralytics import YOLO
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
+from sort.sort import Sort
+from util import get_car, read_license_plate, write_csv
 
-import util
+results = {}
+mot_tracker = Sort()
 
+# 加载模型
+coco_model = YOLO('yolov8n.pt')
+license_plate_detector = YOLO('/Users/david/HProject/Demo/motorcycle_rego/model/license_plate_detector.pt')
 
-# define constants
-model_cfg_path = os.path.join('..','config', 'darknet-yolov3.cfg')
-model_weights_path = os.path.join('..','config', 'model.weights')
-class_names_path = os.path.join('..','config', 'class.names')
+# 加载视频
+# cap = cv2.VideoCapture('/Users/david/HProject/Demo/testSet/sample.mp4')
+cap = cv2.VideoCapture('/Users/david/HProject/Demo/testSet/engroad.MOV')
 
+vehicles = [2, 3, 5, 7]  # 定义车辆类别ID
 
-# img_path = os.path.join('testSet', 'auto.png')
-img_path = os.path.join('..','testSet', 'rav4.jpg')
+# 读取视频帧
+frame_nmr = -1
+ret = True
+while ret:
+    frame_nmr += 1
+    ret, frame = cap.read()
+    if ret:    
+        results[frame_nmr] = {}
+        # 检测车辆
+        detections = coco_model(frame)[0]
+        detections_ = []
+        for detection in detections.boxes.data.tolist():
+            x1, y1, x2, y2, score, class_id = detection
+            if int(class_id) in vehicles:
+                detections_.append([x1, y1, x2, y2, score])
+        
+        # 跟踪车辆        
+        track_ids = mot_tracker.update(np.asarray(detections_))
+        
+        # 检测车牌
+        license_plates = license_plate_detector(frame)[0]
+        for license_plate in license_plates.boxes.data.tolist():
+            x1, y1, x2, y2, score, class_id = license_plate
+        
+            # 分配车牌给车辆
+            xcar1, ycar1, xcar2, ycar2, car_id = get_car(license_plate, track_ids)
+            if car_id != -1:
+                # 裁剪车牌区域
+                license_plate_crop = frame[int(y1):int(y2), int(x1): int(x2), :]
+        
+                # 处理车牌图像
+                license_plate_crop_gray = cv2.cvtColor(license_plate_crop, cv2.COLOR_BGR2GRAY)
+                _, license_plate_crop_thresh = cv2.threshold(license_plate_crop_gray, 64, 255, cv2.THRESH_BINARY_INV)
+                
+                # 读取车牌号码
+                license_plate_text, license_plate_text_score = read_license_plate(license_plate_crop_thresh)
+                
+                if license_plate_text is not None:
+                    results[frame_nmr][car_id] = {'car': {'bbox': [xcar1, ycar1, xcar2, ycar2]},
+                                                  'license_plate': {'bbox': [x1, y1, x2, y2],
+                                                                    'text': license_plate_text,
+                                                                    'bbox_score': score,
+                                                                    'text_score': license_plate_text_score}}
+                
+# 创建目录（如果不存在）
+output_dir = '/Users/david/HProject/Demo/excel_result'
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
 
-# load class names
-with open(class_names_path, 'r') as f:
-    class_names = [j[:-1] for j in f.readlines() if len(j) > 2]
-    f.close()
-
-# load model
-net = cv2.dnn.readNetFromDarknet(model_cfg_path, model_weights_path)
-
-# load image
-
-img = cv2.imread(img_path)
-
-H, W, _ = img.shape
-
-# convert image
-blob = cv2.dnn.blobFromImage(img, 1 / 255, (320, 320), (0, 0, 0), True)
-
-# get detections
-net.setInput(blob)
-
-detections = util.get_outputs(net)
-
-# bboxes, class_ids, confidences
-bboxes = []
-class_ids = []
-scores = []
-
-for detection in detections:
-    # [x1, x2, x3, x4, x5, x6, ..., x85]
-    bbox = detection[:4]
-
-    xc, yc, w, h = bbox
-    bbox = [int(xc * W), int(yc * H), int(w * W), int(h * H)]
-
-    bbox_confidence = detection[4]
-
-    class_id = np.argmax(detection[5:])
-    score = np.amax(detection[5:])
-
-    bboxes.append(bbox)
-    class_ids.append(class_id)
-    scores.append(score)
-
-# apply nms
-bboxes, class_ids, scores = util.NMS(bboxes, class_ids, scores)
-
-# plot
-
-for bbox_, bbox in enumerate(bboxes):
-    xc, yc, w, h = bbox
-
-    """
-    cv2.putText(img,
-                class_names[class_ids[bbox_]],
-                (int(xc - (w / 2)), int(yc + (h / 2) - 20)),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                7,
-                (0, 255, 0),
-                15)
-    """
-    img = cv2.rectangle(img,
-                        (int(xc - (w / 2)), int(yc - (h / 2))),
-                        (int(xc + (w / 2)), int(yc + (h / 2))),
-                        (0, 255, 0),
-                        10)
-
-plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-plt.show()
+# 写入结果到CSV文件
+write_csv(results, os.path.join(output_dir, 'test.csv'))
--- motorcycle_rego/util.py
+++ motorcycle_rego/util.py
@@ -1,66 +1,195 @@
-import numpy as np
-import cv2
+import string
+import easyocr
+
+# Initialize the OCR reader
+reader = easyocr.Reader(['en'], gpu=False)
+
+# Mapping dictionaries for character conversion
+dict_char_to_int = {'O': '0',
+                    'I': '1',
+                    'J': '3',
+                    'A': '4',
+                    'G': '6',
+                    'S': '5'}
+
+dict_int_to_char = {'0': 'O',
+                    '1': 'I',
+                    '3': 'J',
+                    '4': 'A',
+                    '6': 'G',
+                    '5': 'S'}
 
 
-def NMS(boxes, class_ids, confidences, overlapThresh = 0.5):
+def write_csv(results, output_path):
+    """
+    Write the results to a CSV file.
 
-    boxes = np.asarray(boxes)
-    class_ids = np.asarray(class_ids)
-    confidences = np.asarray(confidences)
+    Args:
+        results (dict): Dictionary containing the results.
+        output_path (str): Path to the output CSV file.
+    """
+    with open(output_path, 'w') as f:
+        f.write('{},{},{},{},{},{},{}\n'.format('frame_nmr', 'car_id', 'car_bbox',
+                                                'license_plate_bbox', 'license_plate_bbox_score', 'license_number',
+                                                'license_number_score'))
 
-    # Return empty lists, if no boxes given
-    if len(boxes) == 0:
-        return [], [], []
+        for frame_nmr in results.keys():
+            for car_id in results[frame_nmr].keys():
+                print(results[frame_nmr][car_id])
+                if 'car' in results[frame_nmr][car_id].keys() and \
+                   'license_plate' in results[frame_nmr][car_id].keys() and \
+                   'text' in results[frame_nmr][car_id]['license_plate'].keys():
+                    f.write('{},{},{},{},{},{},{}\n'.format(frame_nmr,
+                                                            car_id,
+                                                            '[{} {} {} {}]'.format(
+                                                                results[frame_nmr][car_id]['car']['bbox'][0],
+                                                                results[frame_nmr][car_id]['car']['bbox'][1],
+                                                                results[frame_nmr][car_id]['car']['bbox'][2],
+                                                                results[frame_nmr][car_id]['car']['bbox'][3]),
+                                                            '[{} {} {} {}]'.format(
+                                                                results[frame_nmr][car_id]['license_plate']['bbox'][0],
+                                                                results[frame_nmr][car_id]['license_plate']['bbox'][1],
+                                                                results[frame_nmr][car_id]['license_plate']['bbox'][2],
+                                                                results[frame_nmr][car_id]['license_plate']['bbox'][3]),
+                                                            results[frame_nmr][car_id]['license_plate']['bbox_score'],
+                                                            results[frame_nmr][car_id]['license_plate']['text'],
+                                                            results[frame_nmr][car_id]['license_plate']['text_score'])
+                            )
+        f.close()
 
-    x1 = boxes[:, 0] - (boxes[:, 2] / 2)  # x coordinate of the top-left corner
-    y1 = boxes[:, 1] - (boxes[:, 3] / 2)  # y coordinate of the top-left corner
-    x2 = boxes[:, 0] + (boxes[:, 2] / 2)  # x coordinate of the bottom-right corner
-    y2 = boxes[:, 1] + (boxes[:, 3] / 2)  # y coordinate of the bottom-right corner
+def license_complies_format(text):
+    """
+    Check if the license plate text complies with the required format.
 
-    areas = (x2 - x1 + 1) * (y2 - y1 + 1)
+    Args:
+        text (str): License plate text.
 
-    indices = np.arange(len(x1))
-    for i, box in enumerate(boxes):
-        # Create temporary indices
-        temp_indices = indices[indices != i]
-        # Find out the coordinates of the intersection box
-        xx1 = np.maximum(box[0] - (box[2] / 2), boxes[temp_indices, 0] - (boxes[temp_indices, 2] / 2))
-        yy1 = np.maximum(box[1] - (box[3] / 2), boxes[temp_indices, 1] - (boxes[temp_indices, 3] / 2))
-        xx2 = np.minimum(box[0] + (box[2] / 2), boxes[temp_indices, 0] + (boxes[temp_indices, 2] / 2))
-        yy2 = np.minimum(box[1] + (box[3] / 2), boxes[temp_indices, 1] + (boxes[temp_indices, 3] / 2))
+    Returns:
+        bool: True if the license plate complies with the format, False otherwise.
+    """
+    if len(text) not in [5, 6]:
+        return False
 
-        w = np.maximum(0, xx2 - xx1 + 1)
-        h = np.maximum(0, yy2 - yy1 + 1)
+    for char in text:
+        if char not in string.ascii_uppercase and char not in string.digits:
+            return False
 
-        # compute the ratio of overlap
-        overlap = (w * h) / areas[temp_indices]
-        # if overlapping greater than our threshold, remove the bounding box
-        if np.any(overlap) > overlapThresh:
-            indices = indices[indices != i]
+    return True
 
-    # return only the boxes at the remaining indices
-    return boxes[indices], class_ids[indices], confidences[indices]
+def format_license(text):
+    """
+    Format the license plate text by converting characters using the mapping dictionaries.
+
+    Args:
+        text (str): License plate text.
+
+    Returns:
+        str: Formatted license plate text.
+    """
+    license_plate_ = ''
+    for char in text:
+        if char in dict_int_to_char:
+            license_plate_ += dict_int_to_char[char]
+        elif char in dict_char_to_int:
+            license_plate_ += dict_char_to_int[char]
+        else:
+            license_plate_ += char
+
+    return license_plate_
+# def license_complies_format(text):
+#     """
+#     Check if the license plate text complies with the required format.
+
+#     Args:
+#         text (str): License plate text.
+
+#     Returns:
+#         bool: True if the license plate complies with the format, False otherwise.
+#     """
+#     if len(text) != 7:
+#         return False
+
+#     if (text[0] in string.ascii_uppercase or text[0] in dict_int_to_char.keys()) and \
+#        (text[1] in string.ascii_uppercase or text[1] in dict_int_to_char.keys()) and \
+#        (text[2] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[2] in dict_char_to_int.keys()) and \
+#        (text[3] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[3] in dict_char_to_int.keys()) and \
+#        (text[4] in string.ascii_uppercase or text[4] in dict_int_to_char.keys()) and \
+#        (text[5] in string.ascii_uppercase or text[5] in dict_int_to_char.keys()) and \
+#        (text[6] in string.ascii_uppercase or text[6] in dict_int_to_char.keys()):
+#         return True
+#     else:
+#         return False
 
 
-def get_outputs(net):
+# def format_license(text):
+#     """
+#     Format the license plate text by converting characters using the mapping dictionaries.
 
-    layer_names = net.getLayerNames()
+#     Args:
+#         text (str): License plate text.
 
-    output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
+#     Returns:
+#         str: Formatted license plate text.
+#     """
+#     license_plate_ = ''
+#     mapping = {0: dict_int_to_char, 1: dict_int_to_char, 4: dict_int_to_char, 5: dict_int_to_char, 6: dict_int_to_char,
+#                2: dict_char_to_int, 3: dict_char_to_int}
+#     for j in [0, 1, 2, 3, 4, 5, 6]:
+#         if text[j] in mapping[j].keys():
+#             license_plate_ += mapping[j][text[j]]
+#         else:
+#             license_plate_ += text[j]
 
-    outs = net.forward(output_layers)
-
-    outs = [c for out in outs for c in out if c[4] > 0.1]
-
-    return outs
+#     return license_plate_
 
 
-def draw(bbox, img):
+def read_license_plate(license_plate_crop):
+    """
+    Read the license plate text from the given cropped image.
 
-    xc, yc, w, h = bbox
-    img = cv2.rectangle(img,
-                        (xc - int(w / 2), yc - int(h / 2)),
-                        (xc + int(w / 2), yc + int(h / 2)),
-                        (0, 255, 0), 20)
+    Args:
+        license_plate_crop (PIL.Image.Image): Cropped image containing the license plate.
 
-    return img
+    Returns:
+        tuple: Tuple containing the formatted license plate text and its confidence score.
+    """
+
+    detections = reader.readtext(license_plate_crop)
+
+    for detection in detections:
+        bbox, text, score = detection
+
+        text = text.upper().replace(' ', '')
+
+        if license_complies_format(text):
+            return format_license(text), score
+
+    return None, None
+
+
+def get_car(license_plate, vehicle_track_ids):
+    """
+    Retrieve the vehicle coordinates and ID based on the license plate coordinates.
+
+    Args:
+        license_plate (tuple): Tuple containing the coordinates of the license plate (x1, y1, x2, y2, score, class_id).
+        vehicle_track_ids (list): List of vehicle track IDs and their corresponding coordinates.
+
+    Returns:
+        tuple: Tuple containing the vehicle coordinates (x1, y1, x2, y2) and ID.
+    """
+    x1, y1, x2, y2, score, class_id = license_plate
+
+    foundIt = False
+    for j in range(len(vehicle_track_ids)):
+        xcar1, ycar1, xcar2, ycar2, car_id = vehicle_track_ids[j]
+
+        if x1 > xcar1 and y1 > ycar1 and x2 < xcar2 and y2 < ycar2:
+            car_indx = j
+            foundIt = True
+            break
+
+    if foundIt:
+        return vehicle_track_ids[car_indx]
+
+    return -1, -1, -1, -1, -1
